96fee852a7e550c6ffb9adba4ec44731ea909529
Lina <107427736+Lina-cpp@users.noreply.github.com>
2025-03-12 22:53:23 +0100

    calc enemy and hit dotproduct

--- Source/WindAndTruth/Private/Enemy/Enemy.cpp
+++ Source/WindAndTruth/Private/Enemy/Enemy.cpp
@@ -5,6 +5,7 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "WindAndTruth/DebugMacros.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 AEnemy::AEnemy()
 {
@@ -47,8 +48,36 @@ void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void AEnemy::GetHit(const FVector& ImpactPoint)
 {
 	DRAW_SPHERE_COLOR(ImpactPoint, FColor::Orange);
-	//GEngine->AddOnScreenDebugMessage(12, 2, FColor::Cyan, FString("Macro activ"));
 	PlayHitReactMontage(FName("FromLeft"));
+
+	const FVector Forward = GetActorForwardVector(); // get enemy's Forward Vector (it's already normalized)
+	const FVector ImpactLowered(ImpactPoint.X, ImpactPoint.Y, GetActorLocation().Z); //debug for arrows
+	const FVector ToHit = (ImpactLowered - GetActorLocation()).GetSafeNormal(); //Vector location of ImpactPoint and ActorLocation (for DotProduct to calculate which anim to play)
+																			 // .GetSafeNormal to normalize vector
+	// Forward * ToHit = |Forward||ToHit| * cos(theta)
+	// |Forward| = 1, |ToHit| = 1, so Forward * ToHit = cos(theta)
+	const double CosTheta = FVector::DotProduct(Forward, ToHit); //save DotProduct to Var
+	//Take the inverse cosine (arc-cosine) of cos(theta) to get theta
+	double Theta = FMath::Acos(CosTheta); //Angle in radians
+	Theta = FMath::RadiansToDegrees(Theta); //Convert radians to Degrees
+	
+	//if crossprduct points down, Theta should be negative
+	const FVector CrossProduct = FVector::CrossProduct(Forward, ToHit);
+	if (CrossProduct.Z < 0)
+	{
+		Theta *= -1.f;
+	}
+	UKismetSystemLibrary::DrawDebugArrow(this, GetActorLocation(), GetActorLocation() + CrossProduct * 100, 15.f, FColor::Blue, 5.f );
+
+/*
+* Debug
+*/
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 5, FColor::Red, FString::Printf(TEXT("Theta: %f"), Theta));
+	}
+	UKismetSystemLibrary::DrawDebugArrow(this, GetActorLocation(), GetActorLocation()+Forward*60.f, 15.f, FColor::Red, 5.f );
+	UKismetSystemLibrary::DrawDebugArrow(this, GetActorLocation(), GetActorLocation() + ToHit * 60.f, 15.f, FColor::Green, 5.f);
 }
 
 
